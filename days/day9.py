1fc30265deb7a92146eceabc4b7486796a7fcc53
Kevin Alberts <kevin@kevinalberts.nl>
2019-12-09 07:22:19 +0100

    Day9: Include own defaultlist implementation instead of relying on libraries

--- days/day9.py
+++ days/day9.py
@@ -1,11 +1,25 @@
 from queue import Queue
 from typing import List
-from defaultlist import defaultlist
 
 from days import AOCDay, day
 
 DEBUG = False
 INFO = False
+
+
+class defaultlist(list):
+    def __init__(self, fx):
+        self._fx = fx
+    def _fill(self, index):
+        while len(self) <= index:
+            self.append(self._fx())
+    def __setitem__(self, index, value):
+        self._fill(index)
+        list.__setitem__(self, index, value)
+    def __getitem__(self, index):
+        self._fill(index)
+        return list.__getitem__(self, index)
+
 
 def add(pc, xarg, yarg, addrarg):
     xmode, x = xarg
