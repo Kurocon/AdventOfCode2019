c1714e60181b8a21d47d26fa4b63330273e56e5f
Kevin Alberts <kevin@kevinalberts.nl>
2019-12-11 06:33:54 +0100

    Added day 11 solutions

--- days/_template.py
+++ days/_template.py
@@ -1,10 +1,13 @@
 from days import AOCDay, day
 
+DEBUG = True
 
 @day(0)
 class DayTemplate(AOCDay):
+    test_input = """""""".split("\n")
+
     def common(self, input_data):
-        pass
+        input_data = self.test_input
 
     def part1(self, input_data):
         pass
---
+++ days/day11.py
@@ -0,0 +1,433 @@
+from collections import defaultdict
+from queue import Queue
+from typing import List
+
+from days import AOCDay, day
+
+DEBUG = False
+INFO = False
+
+
+class defaultlist(list):
+    def __init__(self, fx):
+        self._fx = fx
+    def _fill(self, index):
+        while len(self) <= index:
+            self.append(self._fx())
+    def __setitem__(self, index, value):
+        self._fill(index)
+        list.__setitem__(self, index, value)
+    def __getitem__(self, index):
+        self._fill(index)
+        return list.__getitem__(self, index)
+
+
+def add(pc, xarg, yarg, addrarg):
+    xmode, x = xarg
+    ymode, y = yarg
+    addrmode, addr = addrarg
+    if xmode == MODE_POS:
+        x = pc.memory[x]
+    elif xmode == MODE_REL:
+        x = pc.memory[pc.relative_base + x]
+    if ymode == MODE_POS:
+        y = pc.memory[y]
+    elif ymode == MODE_REL:
+        y = pc.memory[pc.relative_base + y]
+    if addrmode == MODE_REL:
+        addr = pc.relative_base + addr
+
+    if DEBUG:
+        print("/ add {} {} ".format(x, y), end="")
+    pc.memory[addr] = x + y
+    if DEBUG:
+        print("=> @{}={}".format(addr, pc.memory[addr]))
+
+def mult(pc, xarg, yarg, addrarg):
+    xmode, x = xarg
+    ymode, y = yarg
+    addrmode, addr = addrarg
+    if xmode == MODE_POS:
+        x = pc.memory[x]
+    elif xmode == MODE_REL:
+        x = pc.memory[pc.relative_base + x]
+    if ymode == MODE_POS:
+        y = pc.memory[y]
+    elif ymode == MODE_REL:
+        y = pc.memory[pc.relative_base + y]
+    if addrmode == MODE_REL:
+        addr = pc.relative_base + addr
+
+    if DEBUG:
+        print("/ mul {} {} ".format(x, y), end="")
+    pc.memory[addr] = x * y
+    if DEBUG:
+        print("=> @{}={}".format(addr, pc.memory[addr]))
+
+def i_input(pc: 'VirtualMachine', addrarg):
+    addrmode, addr = addrarg
+    if addrmode == MODE_REL:
+        addr = pc.relative_base + addr
+    if DEBUG:
+        print("/ inp {} ".format(addr))
+    if pc.input_queue.empty():
+        if INFO or DEBUG:
+            print("Waiting for input...")
+        return True
+    else:
+        if INFO or DEBUG:
+            print("Inputting...")
+        value = pc.input_queue.get()
+        pc.memory[addr] = value
+    return False
+
+def output(pc: 'VirtualMachine', addrarg):
+    addrmode, addr = addrarg
+    if addrmode == MODE_POS:
+        addr = pc.memory[addr]
+    elif addrmode == MODE_REL:
+        addr = pc.memory[pc.relative_base + addr]
+    if DEBUG:
+        print("/ out {} ".format(addr))
+    if INFO or DEBUG:
+        print("Outputting...")
+    pc.output_queue.put(addr)
+
+def jump_if_true(pc, xarg, addrarg):
+    xmode, x = xarg
+    addrmode, addr = addrarg
+    if xmode == MODE_POS:
+        x = pc.memory[x]
+    elif xmode == MODE_REL:
+        x = pc.memory[pc.relative_base + x]
+    if addrmode == MODE_POS:
+        addr = pc.memory[addr]
+    elif addrmode == MODE_REL:
+        addr = pc.memory[pc.relative_base + addr]
+    if DEBUG:
+        print("/ jit {} => {} ".format(x, addr))
+    if x != 0:
+        pc.pc = addr
+
+def jump_if_false(pc, xarg, addrarg):
+    xmode, x = xarg
+    addrmode, addr = addrarg
+    if xmode == MODE_POS:
+        x = pc.memory[x]
+    elif xmode == MODE_REL:
+        x = pc.memory[pc.relative_base + x]
+    if addrmode == MODE_POS:
+        addr = pc.memory[addr]
+    elif addrmode == MODE_REL:
+        addr = pc.memory[pc.relative_base + addr]
+    if DEBUG:
+        print("/ jif {} => {} ".format(x, addr))
+    if x == 0:
+        pc.pc = addr
+
+def less_than(pc, xarg, yarg, addrarg):
+    xmode, x = xarg
+    ymode, y = yarg
+    addrmode, addr = addrarg
+    if xmode == MODE_POS:
+        x = pc.memory[x]
+    elif xmode == MODE_REL:
+        x = pc.memory[pc.relative_base + x]
+    if ymode == MODE_POS:
+        y = pc.memory[y]
+    elif ymode == MODE_REL:
+        y = pc.memory[pc.relative_base + y]
+    if addrmode == MODE_REL:
+        addr = pc.relative_base + addr
+    if DEBUG:
+        print("/ lt {} {} => {} ".format(x, y, addr))
+    if x < y:
+        pc.memory[addr] = 1
+    else:
+        pc.memory[addr] = 0
+
+def equals(pc, xarg, yarg, addrarg):
+    xmode, x = xarg
+    ymode, y = yarg
+    addrmode, addr = addrarg
+    if xmode == MODE_POS:
+        x = pc.memory[x]
+    elif xmode == MODE_REL:
+        x = pc.memory[pc.relative_base + x]
+    if ymode == MODE_POS:
+        y = pc.memory[y]
+    elif ymode == MODE_REL:
+        y = pc.memory[pc.relative_base + y]
+    if addrmode == MODE_REL:
+        addr = pc.relative_base + addr
+    if DEBUG:
+        print("/ eq {} {} => {} ".format(x, y, addr))
+    if x == y:
+        pc.memory[addr] = 1
+    else:
+        pc.memory[addr] = 0
+
+def rebase(pc, xarg):
+    xmode, x = xarg
+    if xmode == MODE_POS:
+        x = pc.memory[x]
+    elif xmode == MODE_REL:
+        x = pc.memory[pc.relative_base + x]
+    if DEBUG:
+        print("/ reb {} => {} ".format(x, pc.relative_base + x))
+    pc.relative_base += x
+
+
+INSTR_ADD = 1
+INSTR_MUL = 2
+INSTR_INP = 3
+INSTR_OUT = 4
+INSTR_JIT = 5
+INSTR_JIF = 6
+INSTR_LT = 7
+INSTR_EQ = 8
+INSTR_REB = 9
+INSTR_END = 99
+
+MODE_POS = 0
+MODE_IMM = 1
+MODE_REL = 2
+
+
+class VirtualMachine:
+    pc = 0
+    memory = defaultlist(lambda: 0)
+    initial_memory = defaultlist(lambda: 0)
+    input_queue: Queue = None
+    output_queue: Queue = None
+    waiting = False
+    relative_base = 0
+    instr_set = {
+        # CONST: (func, nargs, name)
+        INSTR_ADD: (add, 3, "add"),
+        INSTR_MUL: (mult, 3, "mult"),
+        INSTR_INP: (i_input, 1, "in"),
+        INSTR_OUT: (output, 1, "out"),
+        INSTR_JIT: (jump_if_true, 2, "jit"),
+        INSTR_JIF: (jump_if_false, 2, "jif"),
+        INSTR_LT: (less_than, 3, "lt"),
+        INSTR_EQ: (equals, 3, "eq"),
+        INSTR_REB: (rebase, 1, "reb"),
+
+        INSTR_END: (lambda: None, 0, "end"),
+    }
+
+    def reset(self):
+        self.memory = defaultlist(0)
+        for i in self.initial_memory:
+            self.memory.append(i)
+        while not self.input_queue.empty():
+            self.input_queue.get()
+        while not self.output_queue.empty():
+            self.output_queue.get()
+        self.waiting = False
+        self.pc = 0
+        self.relative_base = 0
+
+    def initialize(self):
+        self.pc = 0
+        self.relative_base = 0
+
+    def run_instruction(self):
+        instr = self.memory[self.pc]
+        if self.waiting and not self.input_queue.empty():
+            if INFO or DEBUG:
+                print("Got input!")
+            self.waiting = False
+        if not self.waiting and instr != INSTR_END:
+            if DEBUG:
+                print("{}: ".format(self.pc), end="")
+
+            instr_opcode = int(str(instr)[-2:])
+            instr_modes = [int(x) for x in reversed(str(instr)[:-2])]
+            instr_func, instr_narg, instr_name = self.instr_set[instr_opcode]
+            while len(instr_modes) < instr_narg:
+                instr_modes.append(MODE_POS)  # Default mode
+
+            if DEBUG:
+                print("{} ".format(instr_name), end="")
+                for i in range(instr_narg):
+                    mode = "I" if instr_modes[i] == MODE_IMM else "@" if instr_modes[i] == MODE_POS else "#"
+                    print("{}{} ".format(mode, self.memory[self.pc + 1 + i]), end="")
+
+            instr_args = [(instr_modes[i], self.memory[self.pc + 1 + i]) for i in range(instr_narg)]
+
+            pc_pre = self.pc
+            res = instr_func(self, *instr_args)
+            if res:
+                self.waiting = True
+            else:
+                if pc_pre == self.pc:
+                    self.pc += 1 + instr_narg
+            return False
+        elif not self.waiting:
+            return True
+        else:
+            return False
+
+    def run_program(self):
+        self.initialize()
+
+        while not self.run_instruction():
+            pass
+
+        return self.memory[0]
+
+BLACK = 0
+WHITE = 1
+
+LEFT = 0
+RIGHT = 1
+
+NORTH = 0
+EAST = 1
+SOUTH = 2
+WEST = 3
+
+@day(11)
+class Day11(AOCDay):
+    virtual_machines: List[VirtualMachine] = []
+    test_input = """109,1,204,-1,1001,100,1,100,1008,100,16,101,1006,101,0,99"""
+    test_input2 = """1102,34915192,34915192,7,4,7,99,0"""
+    test_input3 = """104,1125899906842624,99"""
+    num_vms = 1
+
+    hull = defaultdict(lambda: (BLACK, False))
+    position = (0, 0)
+
+    direction = NORTH
+
+    def common(self, input_data):
+        self.memory = list(map(int, input_data.split(',')))
+        self.virtual_machines = []
+        self.hull = defaultdict(lambda: (BLACK, False))
+        self.position = (0, 0)
+        self.direction = NORTH
+        for i in range(self.num_vms):
+            vm = VirtualMachine()
+            vm.input_queue = Queue()
+            vm.output_queue = Queue()
+            for i in self.memory:
+                vm.memory.append(i)
+                vm.initial_memory.append(i)
+            self.virtual_machines.append(vm)
+
+    def cur_color(self):
+        return self.hull[self.position][0]
+
+    def has_painted(self):
+        return self.hull[self.position][1]
+
+    def move(self, move):
+        directions = {
+            (NORTH, LEFT): WEST,
+            (EAST, LEFT): NORTH,
+            (SOUTH, LEFT): EAST,
+            (WEST, LEFT): SOUTH,
+
+            (NORTH, RIGHT): EAST,
+            (EAST, RIGHT): SOUTH,
+            (SOUTH, RIGHT): WEST,
+            (WEST, RIGHT): NORTH,
+        }
+        positions = {
+            NORTH: (0, -1),
+            EAST: (1, 0),
+            SOUTH: (0, 1),
+            WEST: (-1, 0),
+        }
+
+        self.direction = directions[(self.direction, move)]
+        self.position = (self.position[0] + positions[self.direction][0], self.position[1] + positions[self.direction][1])
+
+    def part1(self, input_data):
+        vm_inputs = [[self.cur_color()]]
+
+        for i, vm_input in enumerate(vm_inputs):
+            if INFO or DEBUG:
+                print("-- Input {} --".format(vm_input))
+
+            for j in vm_input:
+                self.virtual_machines[i].input_queue.put(j)
+
+        states = [False for _ in self.virtual_machines]
+        if INFO or DEBUG:
+            print("Running virtual machines...")
+        color, direction = None, None
+        while not all(states):
+            for i, vm in enumerate(self.virtual_machines):
+                try:
+                    states[i] = vm.run_instruction()
+                except Exception as e:
+                    print("=== EXCEPTION ===")
+                    print("PC:", vm.pc)
+                    print("Memory", vm.memory)
+                    raise e
+
+                # On output, move robot and get new position as input
+                if not vm.output_queue.empty() and color is None:
+                    color = vm.output_queue.get()
+                elif not vm.output_queue.empty() and direction is None:
+                    direction = vm.output_queue.get()
+                if color is not None and direction is not None:
+                    if INFO or DEBUG:
+                        print("Output = {}".format((color, direction)))
+                    self.hull[self.position] = (color, True)
+                    self.move(direction)
+                    vm.input_queue.put(self.cur_color())
+                    color, direction = None, None
+
+        yield sum([1 for x in self.hull.values() if x[1]])
+
+    def part2(self, input_data):
+        self.hull[self.position] = (WHITE, False)
+        vm_inputs = [[self.cur_color()]]
+
+        for i, vm_input in enumerate(vm_inputs):
+            if INFO or DEBUG:
+                print("-- Input {} --".format(vm_input))
+
+            for j in vm_input:
+                self.virtual_machines[i].input_queue.put(j)
+
+        states = [False for _ in self.virtual_machines]
+        if INFO or DEBUG:
+            print("Running virtual machines...")
+        color, direction = None, None
+        while not all(states):
+            for i, vm in enumerate(self.virtual_machines):
+                try:
+                    states[i] = vm.run_instruction()
+                except Exception as e:
+                    print("=== EXCEPTION ===")
+                    print("PC:", vm.pc)
+                    print("Memory", vm.memory)
+                    raise e
+
+                # On output, move robot and get new position as input
+                if not vm.output_queue.empty() and color is None:
+                    color = vm.output_queue.get()
+                elif not vm.output_queue.empty() and direction is None:
+                    direction = vm.output_queue.get()
+                if color is not None and direction is not None:
+                    if INFO or DEBUG:
+                        print("Output = {}".format((color, direction)))
+                    self.hull[self.position] = (color, True)
+                    self.move(direction)
+                    vm.input_queue.put(self.cur_color())
+                    color, direction = None, None
+
+        min_x = min(x[0] for x in self.hull.keys())
+        max_x = max(x[0] for x in self.hull.keys())
+        min_y = min(x[1] for x in self.hull.keys())
+        max_y = max(x[1] for x in self.hull.keys())
+
+        for j in range(min_y, max_y + 1):
+            for i in range(min_x, max_x + 1):
+                print("#" if self.hull[(i, j)][0] == WHITE else " ", end="")
+            print("")
